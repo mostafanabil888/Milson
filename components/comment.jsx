52c371418da98e0ee9ca50c486aca973b9c3a552
mostafanabil888 <sasanabil420@gmail.com>
2023-02-21 11:30:03 +0200

    final

--- components/comment.jsx
+++ components/comment.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import Styles from '../styles/Comment.module.scss'
+import React from "react"
+import Styles from "../styles/Comment.module.scss"
 export default function comment() {
   return (
     <div className={Styles.comment}>
         <div className="container text-center">
-            <p>I was so excited to start learning English, but I quickly realized I didn't follow the right methods to reach anywhere near my target level. 
-                Monglish delivers everything I need for my advancement easily.</p>
+            <p>I was so excited to start learning English, but I quickly realized I did not follow the right methods to reach anywhere near my target level. 
+                Melsion delivers everything I need for my advancement easily.</p>
         </div>
     </div>
   )
