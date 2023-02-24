b866c67ee1749b3f1645bb8ca72d61780993cfea
mostafanabil888 <sasanabil420@gmail.com>
2023-02-24 22:24:39 +0200

    final deploy

--- components/Video.jsx
+++ components/Video.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import ReactPlayer from 'react-player/youtube'
-import styles from '../styles/Video.module.scss'
 export default function Video() {
   return (
     <div className='container py-5'>
       <div className="d-flex align-items-center justify-content-center">
-        {/* <ReactPlayer  url='https://www.youtube.com/watch?v=ysz5S6PUM-U' /> */}
+        <ReactPlayer  url='https://www.youtube.com/watch?v=ysz5S6PUM-U' />
       </div>
       <p className='my-5  fw-bold '>Milson Academy applies the best world-class educational pedagogy, namely the immersion approach towards language acquisition. Now, wherever you are, we can reach you to make your dream of learning English true.</p>
       <p className='fw-bold'>Milson Academy applies the best world-class educational pedagogy, namely the immersion approach towards language acquisition. Now, wherever you are, we can reach you to make your dream of learning English true.
