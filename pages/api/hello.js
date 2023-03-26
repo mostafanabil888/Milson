d933d2cafe74f2cd7d072c3f6dbbf4e456812320
mostafanabil888 <sasanabil420@gmail.com>
2023-03-26 22:21:49 +0200

    login

--- pages/api/hello.js
+++
@@ -1,9 +0,0 @@
-export default async function handler(req, res) {
-  const { email, password } = req.body;
-  // Check if user email and password are valid
-  if (email === 'user@example.com' && password === 'password123') {
-    res.status(200).json({ success: true });
-  } else {
-    res.status(401).json({ success: false, message: 'Invalid email or password' });
-  }
-}
